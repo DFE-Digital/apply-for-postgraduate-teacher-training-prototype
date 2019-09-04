7a9dffc32f62879a8776cbd413c11ba70bebc537
Paul Hayes <fofr@users.noreply.github.com>
2019-09-04 14:46:30 +0100

    WIP

--- app/routes/application/personal-statement.js
+++ app/routes/application/personal-statement.js
@@ -1,20 +1,41 @@
 const providers = require('../../data/providers')
+const utils = require('./../../utils')
 
 /**
  * Application: Subject knowledge routes
  */
 module.exports = router => {
   router.get('/application/:applicationId/personal-statement/:providerCode', (req, res) => {
-    const referrer = req.query.referrer
+    const applicationData = utils.applicationData(req)
     const providerCode = req.params.providerCode
-    const provider = providers[providerCode]
+    const personalStatement = applicationData['personal-statements'][providerCode]
+    // const referrer = req.query.referrer
+    // const provider = providers[providerCode]
 
-    res.render(`application/personal-statement/index`, {
-      formaction: referrer || `/application/${req.params.applicationId}`,
-      referrer,
-      providerCode,
-      provider
-    })
+    if (personalStatement && personalStatement.id) {
+      res.redirect(`/application/${req.params.applicationId}/personal-statement/${providerCode}/review`)
+    } else {
+      res.redirect(`/application/${req.params.applicationId}/personal-statement/${providerCode}/new`)
+    }
+  })
+
+  router.post('/application/:applicationId/personal-statement/:providerCode/copy', (req, res) => {
+    const applicationData = utils.applicationData(req)
+    const providerCode = req.params.providerCode
+    const personalStatement = applicationData['personal-statements'][providerCode]
+    // const referrer = req.query.referrer
+    // const provider = providers[providerCode]
+
+    if (personalStatement['write-new'] == 'new') {
+      // TODO: copy existing
+
+
+    } else {
+      // TODO: set pointer
+      // What does this look like?
+    }
+
+    res.redirect(`/application/${req.params.applicationId}/personal-statement/${providerCode}/review`)
   })
 
   // Render other cover letter pages
